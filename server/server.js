86a6d837f7a350f9eda6201198f295bf750eff55
Mariano Barragán <marianobarragan@buenosaires.gob.ar>
2018-08-10 17:12:28 -0300

    fix for cors

--- server/server.js
+++ server/server.js
@@ -6,14 +6,16 @@ const bodyParser = require('body-parser');
 const express = require('express')
 const app = express()
 
-
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
+require('./middleware/headers.js')(app);
 require('./routes/api.js')(app);
-
+/****************************************************** */
+// var cors = require('cors');
+// app.use(cors({origin: 'localhost:3000'}))
 /****************************************************** */
 // const mongoose = require('mongoose');
 // mongoose.connect(`mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,{ useNewUrlParser: true }, (err,res)=>{
@@ -23,4 +25,5 @@ require('./routes/api.js')(app);
 /****************************************************** */
 app.listen(process.env.PORT, ()=>{
     console.log(`Listening to: ${process.env.PORT}`);
+    //console.log(process.env);
 } );
